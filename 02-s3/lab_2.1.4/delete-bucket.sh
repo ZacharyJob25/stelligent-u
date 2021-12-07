c4aa3066e9f92eda5d79145e5c3abcc003740fed
ZacharyJob25 <zachary_job@outlook.com>
2021-12-07 13:33:52 -0500

    Lab 2.1.4, see notes in script

--- 02-s3/lab_2.1.4/delete-bucket.sh
+++ 02-s3/lab_2.1.4/delete-bucket.sh
@@ -1,3 +1,17 @@
+#Admittedly I got lazy here because I would rather NEVER
+#let this into production. Cloudformation s3 rollback is okay
+#as term prot is an easy guard that isn't restrictive.
+
+aws s3 rm \
+	s3://$1 \
+	--recursive
+
+#aws s3api delete-objects \
+#	--bucket $1 \
+#	--region $2 \
+#	--delete '{"Objects":[{<INSERT AWK>}]}'
+#
+
 aws s3api delete-bucket \
 	--bucket $1 \
 	--region $2
